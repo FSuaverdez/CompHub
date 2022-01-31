5e8ee56f2195e887cb4ee856700ab4c287f41c01
DamnitsAlvin <alvinlim794@gmail.com>
2022-01-31 22:57:56 +0800

    db mods 2

--- source/db_connect.php
+++ source/db_connect.php
@@ -2,6 +2,6 @@
 
     $mysqli = new mysqli('localhost', 'root', '','request') or die(mysqli_error($mysqli));
 
-   # $mysqli = new mysqli('https://cpanel.ite4-bsit-ab.com/cpsess3374708460/3rdparty/phpMyAdmin/', 'xdizhfxe_conn', 'nM1(VqKwkb8N','xdizhfxe_bsit4a') or die(mysqli_error($mysqli));
+   # $mysqli = new mysqli('localhost', 'xdizhfxe_conn', 'nM1(VqKwkb8N','xdizhfxe_bsit4a') or die(mysqli_error($mysqli));
 
 ?>
