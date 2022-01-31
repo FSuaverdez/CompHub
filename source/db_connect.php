4ac59d280c347d1dc9d8a2f87e54a94d3227830b
DamnitsAlvin <alvinlim794@gmail.com>
2022-01-31 22:59:22 +0800

    db mods 2

--- source/db_connect.php
+++ source/db_connect.php
@@ -1,7 +1,9 @@
 <?php 
 
+    #pang local lang to
     $mysqli = new mysqli('localhost', 'root', '','request') or die(mysqli_error($mysqli));
 
+    #pang deployment to
    # $mysqli = new mysqli('localhost', 'xdizhfxe_conn', 'nM1(VqKwkb8N','xdizhfxe_bsit4a') or die(mysqli_error($mysqli));
 
 ?>
